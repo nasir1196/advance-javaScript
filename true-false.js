b7b7d609e5f7ae997b834156c48c001df74a8db7
Nasir Uddin <monforingnasir@gmail.com>
2021-02-02 12:26:57 +0600

    True vs False

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//Falsy:
+//false
+//undefined
+//null
+//0
+//""
+//NaN
+
+//truthy:
+//" ", "0", [], {}, true
+// const names = '0'; //truthy value;
+const names = 0; //falsy value;
+if (names) {
+    console.log("Truthy value");
+} else {
+    console.log("Falsy Value");
+}
